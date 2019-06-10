b22dbb31b031ff39c03c761c5341cb25fdcfa8bc
yckimn <41068173+yckimn@users.noreply.github.com>
2019-06-10 10:36:36 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello {
         public static void main(String[] args) {
-        System.out.println("This is java app \n by using Docker\n(1)");
+        System.out.println("This is java app \n by using Docker\n(2)");
         }
 }
